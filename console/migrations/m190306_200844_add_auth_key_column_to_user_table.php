254e8899b7b54b815584629831b0215f8632f2a6
vagrant <vagrant@phn>
2019-03-06 20:14:58 +0000

    auth_key column added to the user table

---
+++ console/migrations/m190306_200844_add_auth_key_column_to_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding auth_key to table `{{%user}}`.
+ */
+class m190306_200844_add_auth_key_column_to_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%user}}', 'auth_key', $this->string());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%user}}', 'auth_key');
+    }
+}
